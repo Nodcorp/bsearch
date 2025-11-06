8be71b730f4af71efe69d1385c5e42fd1e605bc6
Nodlock <nodlockincorporated@gmail.com>
2025-11-06 16:11:02 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    window.location.href = '/api/proxy';
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <p>Loading Blekko…</p>
+    </div>
+  );
+}
