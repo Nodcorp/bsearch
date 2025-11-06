6a9c38e2c057dabc00e5485b5a145097565e5843
Nodlock <nodlockincorporated@gmail.com>
2025-11-06 16:36:24 +0000

    Update proxy.js

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -3,13 +3,11 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
   let { q } = req.query;
 
-
-const baseHome = 'https://web.archive.org/web/2011020700000/http://blekko.com/';
+  const baseHome = 'https://web.archive.org/web/2011020700000/http://blekko.com';
 
   let targetUrl = baseHome;
-
   if (q) {
-    const [queryPart, slashtag] = q.split('/');
+    const [queryPart, slashtag] = q.split('/', 2);
     const finalQuery = slashtag ? `${queryPart}+site:${slashtag}` : queryPart;
     targetUrl = `${baseHome}/ws/?q=${encodeURIComponent(finalQuery)}`;
   }
@@ -18,11 +16,13 @@ const baseHome = 'https://web.archive.org/web/2011020700000/http://blekko.com/';
     const response = await fetch(targetUrl);
     let html = await response.text();
 
-    html = html.replace(/(href|src)="\/(.*?)"/g, `$1="${baseHome}/$2"`);
+    html = html.replace(/(href|src)="\/(?!ws\/)(.*?)"/g, `$1="${baseHome}/$2"`);
 
-    html = html.replace(/<form([^>]+)action="\/ws\/?"([^>]*)>/g, `<form$1action="/api/proxy"$2>`);
+    html = html.replace(/(href|action)="\/ws\/([^"]*)"/g, `$1="/api/proxy?q=$2"`);
 
     html = html.replace(/<div id="wm-ipp".*?<\/div>/gs, '');
+
+    html = html.replace(/https:\/\/web\.archive\.org\/web\/\d+\/http:\/\/blekko\.com/g, '');
 
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
